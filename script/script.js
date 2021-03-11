21fda369069f9ef17d0be9e79efe0ed7dd3e3ff3
Aleksei-Mac <alexsanatov@gmail.com>
2021-03-11 14:59:23 +0200

    feat: сделать адаптив

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ let popup = document.querySelector('.popup');
 let popupOpen = document.querySelector('.profile__edit-button');
 let popupClose = document.querySelector('.popup__close');
 
+
 popupOpen.addEventListener('click', () => {
   popup.style.display = 'block';
 });
@@ -9,3 +10,25 @@ popupOpen.addEventListener('click', () => {
 popupClose.addEventListener('click', () => {
   popup.style.display = 'none';
 });
+
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__form_name');
+let jobInput = document.querySelector('.popup__form_descr');
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); 
+
+  let nameInputValue = nameInput.value;
+  let jobInputValue = jobInput.value;
+
+  let title = document.querySelector('.profile__title');
+  let subtitle = document.querySelector('.profile__subtitle');
+
+  title.textContent = nameInputValue;
+  subtitle.textContent = jobInputValue;
+
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
