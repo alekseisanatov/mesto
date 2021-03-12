dafc59890b010ac811462e36129f9208608782e8
Aleksei-Mac <alexsanatov@gmail.com>
2021-03-12 14:30:11 +0200

    fix: исправил все ошибки из замечаний

--- script/script.js
+++ script/script.js
@@ -1,34 +1,28 @@
 let popup = document.querySelector('.popup');
 let popupOpen = document.querySelector('.profile__edit-button');
 let popupClose = document.querySelector('.popup__close');
-
-
-popupOpen.addEventListener('click', () => {
-  popup.style.display = 'block';
-});
-
-popupClose.addEventListener('click', () => {
-  popup.style.display = 'none';
-});
-
-
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__name');
 let jobInput = document.querySelector('.popup__descr');
-
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
 
   let nameInputValue = nameInput.value;
   let jobInputValue = jobInput.value;
-
   let title = document.querySelector('.profile__title');
   let subtitle = document.querySelector('.profile__subtitle');
 
   title.textContent = nameInputValue;
   subtitle.textContent = jobInputValue;
-
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler);
+
+popupOpen.addEventListener('click', () => {
+  popup.classList.add('popup_active');
+});
+
+popupClose.addEventListener('click', () => {
+  popup.classList.remove('popup_active');
+});
